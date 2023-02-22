c3e0ee45fda367878d2772537dbbe728d5c89d2b
Jacob Rushlow <gheezl25@gmail.com>
2023-02-21 20:55:01 -0500

    Adding the context to the application

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,20 +1,10 @@
 import { Typography, Box } from "@mui/material";
-import { useEffect } from "react";
-import { useUserContext } from "../../contexts/UserContext";
-
 import { HeaderBorder } from "./header-styles";
 
-
 const Header = () => {
-    const {user, setUser} = useUserContext();
-
-    useEffect(() => {
-        console.log(user, setUser)
-    }, [user])
-
     return(
         <Box sx={HeaderBorder}>
-            <Typography onClick={() => setUser("LOL HI")} variant="h3">Welcome to the weather app</Typography>
+            <Typography  variant="h3">Welcome to the weather app</Typography>
         </Box>
     )
 }
