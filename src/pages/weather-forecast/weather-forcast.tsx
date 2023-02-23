6afab91f3cf09e53a4d61982dc3715cb9f1efbfe
Jacob Rushlow <gheezl25@gmail.com>
2023-02-23 14:24:34 -0500

    Continued filling out the weather forcast page ui, still needs a lot of work

--- src/pages/weather-forecast/weather-forcast.tsx
+++ src/pages/weather-forecast/weather-forcast.tsx
@@ -1,12 +1,14 @@
-import { Grid, Paper, Typography } from "@mui/material";
+import { Box, Grid, Paper, Typography } from "@mui/material";
 import { Container } from "@mui/system";
 import { useEffect, useState } from "react";
 import { BASEURL, WeatherAPiHeaders, APIKEY } from "../../api-services/weather-api/config";
 import { useUserContext } from "../../contexts/UserContext";
 
 const WeatherForcast = () => {
-    const [userLocation, setUserLocation] = useState("Delta")
+    const [userLocation, setUserLocation] = useState("Delta, Ohio")
     const [weather, setWeather] = useState<any>()
+    const [formatedCurrentDate, setFormatedCurrentDate] = useState<string>("")
+    const [formatedCurrentTIme, setFormatedCurrentTime] = useState<string>("")
 
     const {user} = useUserContext()
 
@@ -27,7 +29,9 @@ const WeatherForcast = () => {
               const data = await response.json();
               console.log(data);
               if (mounted) {
-                setWeather(data)
+                setFormatedCurrentTime(getTimeOfDay(data.location.localtime_epoch));
+                setFormatedCurrentDate(formatEpochDate(data.location.localtime_epoch));
+                setWeather(data);
               }
             } catch (error) {
               console.error('Error:', error);
@@ -41,35 +45,87 @@ const WeatherForcast = () => {
       }
     }, [])
 
+    // utility functions
+    const formatEpochDate = (epochTime: number) => {
+      const date = new Date(epochTime * 1000);
+      return date.toLocaleDateString()
+    }
+
+    const getTimeOfDay = (epochTime: number) => {
+      const date = new Date(epochTime * 1000);
+      let hours = date.getHours();
+      let minutes = date.getMinutes().toString();
+
+      const ampm = hours >= 12 ? 'PM' : 'AM';
+      hours = hours % 12;
+      hours = hours ? hours : 12;
+
+      if (minutes.length < 2) {
+        minutes = "0" + minutes;
+      }
+
+      return hours + ':' + minutes + ' ' + ampm;
+    }
+
+    // styles
     const PaperStyling = {
       "padding": "10px",
     }
 
     const GridContainerStyling = {
-      "marginTop": "10px"
+      "marginTop": "5px",
+      "marginBottom": "10px"
     }
 
     return (
       <Container maxWidth="xl">
+        {/* displays the location and current weather */}
         <Grid container sx={GridContainerStyling} spacing={1}>
           <Grid item sm={12} md={6}>
             <Paper sx={PaperStyling} elevation={3}>
-              <Typography >{weather?.location.name}, {weather?.location.region}, {weather?.location.country}</Typography> 
+              <Typography variant="h5">{weather?.location.name}, {weather?.location.region}, {weather?.location.country}</Typography>
+              <Typography>As of {formatedCurrentTIme} on {formatedCurrentDate}</Typography> 
             </Paper>
           </Grid>
           <Grid item sm={12} md={6}>
-            <Paper sx={PaperStyling} elevation={3}>{user?.usesMetric ? `${weather?.current.temp_c} C` : `${weather?.current.temp_f} F`}</Paper>
+            <Paper sx={PaperStyling} elevation={3}>
+              <Grid container>
+                <Typography variant="h2">{weather?.current.condition.text}</Typography>
+                <Typography variant="h2">{user?.usesMetric ? `${weather?.current.temp_c} C` : `${weather?.current.temp_f} F`}</Typography> 
+              </Grid>
+              {/* <Typography variant="h3">Feels like {user?.usesMetric ? `${weather?.current.feelslike_c} C` : `${weather?.current.feelslike_f} F`}</Typography>  */}
+            </Paper>
           </Grid>
         </Grid>
+        {/* displays the 7 day forcast */}
+        <Box>
+          <Typography variant="h4">7 day Forecast</Typography>
+        </Box>
         <Grid container sx={GridContainerStyling} spacing={1}>
-          {weather?.forecast.forecastday.map((day: any) => (
-            <Grid md={1.7142857143} item>
-              <Paper sx={PaperStyling}>
-                <Typography>{day?.date}</Typography>
-              </Paper>
-            </Grid>
-          ))}
-          
+          {
+            weather?.forecast.forecastday.map((day: any) => (
+              <Grid key={day?.date} md={2} item>
+                <Paper sx={PaperStyling}>
+                  <Typography>{day?.date}</Typography>
+                </Paper>
+              </Grid>
+            ))
+          }
+        </Grid>
+        {/* displays the alerts  */}
+        <Box>
+          <Typography variant="h4">Alerts</Typography>
+        </Box>
+        <Grid container sx={GridContainerStyling} spacing={1}>
+          {
+            weather?.alerts.alert.map((alert: any) => (
+              <Grid key={alert?.headline} md={2} item>
+                <Paper sx={PaperStyling}>
+                  <Typography>{alert?.headline}</Typography>
+                </Paper>
+              </Grid>
+            ))
+          }
         </Grid>
       </Container>
     )
