fabc784ae5d1dfaa84da14d6ac92ea3a3521abcd
Jacob Rushlow <gheezl25@gmail.com>
2023-02-23 10:04:28 -0500

    Began filling out the weather forcast page ui

--- src/pages/weather-forecast/weather-forcast.tsx
+++ src/pages/weather-forecast/weather-forcast.tsx
@@ -1,3 +1,4 @@
+import { Grid, Paper } from "@mui/material"
 import { useEffect, useState } from "react"
 import { BASEURL, WeatherAPiHeaders, APIKEY } from "../../api-services/weather-api/config"
 
@@ -5,12 +6,12 @@ const WeatherForcast = () => {
     const [userLocation, setUserLocation] = useState("Delta, Ohio")
 
     useEffect(() => {
-        getCurrentWeather()
+        getWeatherForcast()
     }, [])
 
-    const getCurrentWeather = async () => {
+    const getWeatherForcast = async () => {
         try {
-            const response = await fetch(`${BASEURL}/current.json?key=${APIKEY}&q=${userLocation}&aqi=yes`, {
+            const response = await fetch(`${BASEURL}/forecast.json?key=${APIKEY}&q=${userLocation}&days=7&aqi=yes&alerts=yes`, {
               method: 'GET',
               headers: WeatherAPiHeaders
             });
@@ -27,9 +28,20 @@ const WeatherForcast = () => {
     }
 
     return (
-        <div>
-            <h2>lol hi</h2>
-        </div>
+        <Grid container spacing={3}>
+          <Grid item xs={12} sm={6} md={3}>
+            <Paper elevation={3}>1</Paper>
+          </Grid>
+          <Grid item xs={12} sm={6} md={3}>
+            <Paper elevation={3}>2</Paper>
+          </Grid>
+          <Grid item xs={12} sm={6} md={3}>
+            <Paper elevation={3}>3</Paper>
+          </Grid>
+          <Grid item xs={12} sm={6} md={3}>
+            <Paper elevation={3}>4</Paper>
+          </Grid>
+        </Grid>
     )
 }
 
