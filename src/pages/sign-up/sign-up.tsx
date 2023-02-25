ba737267d4b415a30cd15c3eed611ae6bd75d017
Jacob Rushlow <gheezl25@gmail.com>
2023-02-25 16:08:37 -0500

    Added formik

--- src/pages/sign-up/sign-up.tsx
+++ src/pages/sign-up/sign-up.tsx
@@ -1,8 +1,30 @@
+import Button from '@mui/material/Button';
+import Container from '@mui/material/Container';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
+
 const SignUp = () => {
     return(
-        <div>
-            <h1>Hello this is the sign up</h1>
-        </div>
+        <Container maxWidth="xl">
+            <Formik
+                initialValues={{"First Name": "", "Email": ""}}
+                // validate={values => {
+                // }}
+                onSubmit={(values, { setSubmitting }) => {
+                    setTimeout(() => {
+                        alert(JSON.stringify(values, null, 2));
+                        setSubmitting(false);
+                    }, 400);
+                }}
+            >
+                <Form>
+                    <Field name="First Name"/>
+                    <ErrorMessage name='First Name' />
+                    <Field name="Email" />
+                    <ErrorMessage name="Email" />
+                    <Button type='submit'>Submit</Button>
+                </Form>
+            </Formik>
+        </Container>
     )
 }
 
